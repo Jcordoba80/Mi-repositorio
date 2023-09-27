5d6a7aeea7b845e968cb03d88924558356d45101
Javier Cordoba <jacovi80@gmail.com>
2023-09-26 21:56:08 -0600

    Primer commit de javaAppNB

---
+++ javaAppNB/src/javaappnb/JavaAppNB.java
@@ -0,0 +1,16 @@
+
+
+package javaappnb;
+
+/**
+ *
+ * @author jacov
+ */
+import java.util.Scanner;
+public class JavaAppNB {
+    static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Se ha realizado una confirmación");
+    }
+
+}
